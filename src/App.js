734a4144ef2bf7dc9dc0dc0ed8459b7f281b2d79
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-07-26 15:05:51 -0700

    add counter display

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component{
     this.state = {
       board: ["❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓"],
       treasureLocation: null,
-      trapLocation: null
+      trapLocation: null,
+      counter: 5
     }
   }
 
@@ -34,15 +35,17 @@ class App extends Component{
     }
     
     
-    this.setState({board: newBoard})
+    this.setState({board: newBoard,counter: this.state.counter-1})
     }
   
 
   render(){
-    const {board,treasureLocation,trapLocation} = this.state
+    const {board,treasureLocation,trapLocation,counter} = this.state
     return(
       <>
         <h1>Treasure Hunt Game ({treasureLocation}/{trapLocation})</h1>
+        <p>Click on a square to make a guess. You have 5 guesses to click on the treasure square, but beware the bomb square.</p>
+        <p>you have {counter} guesses left.</p>
         <div className="board">
         {board.map(((square,index) => {
           return <Square key={index} index={index} value={square} handleClick={this.handleClick}/>
