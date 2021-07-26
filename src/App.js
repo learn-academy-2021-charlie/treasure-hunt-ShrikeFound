b0ff176cf1d8720c183672d022b86c94381ef0f4
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-07-26 14:58:45 -0700

    added random treasure location, treasure logic

--- src/App.js
+++ src/App.js
@@ -6,22 +6,36 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓"]
+      board: ["❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓"],
+      treasureLocation: null
     }
   }
 
+  componentDidMount(){
+    let treasureLocation = Math.floor(Math.random() * this.state.board.length)
+    this.setState({treasureLocation: treasureLocation})
+  }
+
+
   handleClick = (index) =>{
+
     const newBoard = [...this.state.board]
-    newBoard[index] = "🌴"
+    if(index === this.state.treasureLocation){
+      newBoard[index] = "💎"
+    }else{
+      newBoard[index] = "🌴"
+    }
+    
+    
     this.setState({board: newBoard})
     }
   
 
   render(){
-    const {board} = this.state
+    const {board,treasureLocation} = this.state
     return(
       <>
-        <h1>Treasure Hunt Game</h1>
+        <h1>Treasure Hunt Game ({treasureLocation})</h1>
         <div className="board">
         {board.map(((square,index) => {
           return <Square key={index} index={index} value={square} handleClick={this.handleClick}/>
