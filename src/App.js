6ca17a6e88694bed7049e46d857deccb5e1fa8f7
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-07-26 14:49:22 -0700

    added squares, styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react'
 import './App.css'
+import Square from './components/Square'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓", "❓"]
     }
   }
 
+  
+
   render(){
+    const {board} = this.state
     return(
       <>
         <h1>Treasure Hunt Game</h1>
+        <div className="board">
+        {board.map(((square,index) => {
+          return <Square key={index} index={index} value={square}/>
+        }))}
+        </div>
       </>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 class Square extends Component{
   render(){
     return(
-      <>
-      </>
+      <div className="square">
+        {this.props.value}
+      </div>
+
     )
   }
 }
